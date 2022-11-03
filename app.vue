b6b3b61f44d813796012d2d0b9732922a5e075c5
IamCharlesM <rafroyevans@gmail.com>
2022-11-02 23:07:31 -0500

    initial commit

---
+++ app.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hero min-h-screen">
+    <div class="hero-overlay bg-opacity-60"></div>
+    <div class="hero-content text-center text-neutral-content">
+      <div class="max-w-md">
+        <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
+        <p class="mb-5">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt,
+          reiciendis laboriosam accusantium velit architecto quasi nostrum
+          dolorum odit explicabo. Sunt, expedita ipsum. Totam aliquid soluta
+          veritatis earum. Animi, maiores necessitatibus.
+        </p>
+        <button class="btn btn-primary">Get Started</button>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style>
+.hero {
+  background-image: url(day-sky.jpg);
+  background-size: auto;
+}
+</style>
