f4c4d74a84979bb538e33e2d90a5ff66209a7137
IamCharlesM <rafroyevans@gmail.com>
2022-11-04 07:51:18 -0500

    Play pause active

--- app.vue
+++ app.vue
@@ -1,23 +1,139 @@
 <template>
   <div class="hero min-h-screen">
     <div class="hero-overlay bg-opacity-60"></div>
-    <div class="hero-content text-center text-neutral-content">
+    <div class="hero-content text-center text-white text-neutral-content">
       <div class="max-w-md">
-        <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
-        <p class="mb-5">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt,
-          reiciendis laboriosam accusantium velit architecto quasi nostrum
-          dolorum odit explicabo. Sunt, expedita ipsum. Totam aliquid soluta
-          veritatis earum. Animi, maiores necessitatibus.
+        <h1 class="mb-5 text-5xl font-bold">{{ info.station.name }}</h1>
+        <img
+          class="min-w-full mb-5"
+          :src="info.now_playing.song.art"
+          alt=""
+          srcset=""
+        />
+        <p class="mb-2 font-bold">
+          {{ info.now_playing.song.artist }}
         </p>
-        <button class="btn btn-primary">Get Started</button>
+        <p>{{ info.now_playing.song.title }}</p>
+        <progress
+          class="progress progress-info"
+          :value="info.now_playing.elapsed"
+          :max="info.now_playing.duration"
+        ></progress>
+        <div class="flex justify-between">
+          {{ info.now_playing.elapsed }} {{ info.now_playing.duration }}
+        </div>
+        <div class="space-x-5">
+          <button class="btn btn-circle btn-outline">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+
+          <audio id="myAudio">
+            <source
+              src="https://edenofthewest.com/listen/eden-radio/radio.mp3"
+              type="audio/mpeg"
+            />
+            Your browser does not support the audio element.
+          </audio>
+          <button
+            v-if="playing === false"
+            class="btn btn-circle btn-outline btn-warning"
+            @click="playAudio()"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+
+          <button
+            v-else
+            class="btn btn-circle btn-outline btn-secondary"
+            @click="pauseAudio()"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+
+          <button class="btn btn-circle btn-outline">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+        </div>
       </div>
     </div>
   </div>
 </template>
-<script>
-export default {};
+<script setup>
+const { data: info } = await useFetch(
+  "https://edenofthewest.com/api/nowplaying/1",
+  {
+    pick: ["station", "now_playing"],
+  }
+);
+
+let playing = ref(false);
+
+const playAudio = () => {
+  let x = document.getElementById("myAudio");
+  x.play();
+  playing.value = true;
+  console.log(playing.value);
+};
+const pauseAudio = () => {
+  let x = document.getElementById("myAudio");
+  x.pause();
+  playing.value = false;
+  console.log(playing.value);
+};
 </script>
+
 <style>
 .hero {
   background-image: url(day-sky.jpg);
