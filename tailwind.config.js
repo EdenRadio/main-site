17a33a9204f7aa3ff2dfead5e315e13211093cbb
IamCharlesM <rafroyevans@gmail.com>
2022-11-13 12:24:18 -0600

    No scrolling on chrome and ios

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./assets/**/*.{vue,js,css}",
+    "./components/**/*.{vue,js}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+  ],
   theme: {
     extend: {},
   },
