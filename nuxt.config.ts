83832d64742ea8f0e3d91a11cfb1f6f998a7f837
IamCharlesM <rafroyevans@gmail.com>
2022-12-14 03:06:14 -0600

    Domain update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -31,6 +31,6 @@ export default defineNuxtConfig({
       albumArt: { modifiers: { format: "webp", width: 400, height: 400 } },
       general: { modifiers: { format: "webp" } },
     },
-    domains: ["edenofthewest.com", "localhost"],
+    domains: ["edenofthewest.com"],
   },
 });
